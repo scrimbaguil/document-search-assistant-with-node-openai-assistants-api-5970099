5bc30c4219aebf54d8086768b2fa821d461a0a37
Guil H <g.hernandez07@gmail.com>
2025-01-27 08:57:23 -0500

    Add files via upload

--- assistant.js
+++ assistant.js
@@ -1,3 +1,21 @@
 // Script to create the assistant
-const openai = require('./config');
-const fs = require('fs');
+const openai = require("./config");
+const assistantId = "";
+
+// create assistant
+async function createAssistant() {
+  try {
+    const response = await openai.beta.assistants.create({
+      name: "Document Search",
+      model: "gpt-4o",
+      instructions:
+        "You are a document search assistant. Answer user queries with relevant information from the documents, or politely explain if none is found.",
+    });
+
+    console.log("Assistant created with ID: ", response.id);
+  } catch (error) {
+    console.error("Error creating assistant:", error.message);
+  }
+}
+
+// createAssistant();
